0459afd87deeda20e7cdfd18e5baa86a5f0cb7de
Saint-Jean Germila <germila@noukod.com>
2019-03-27 15:40:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 function init() {
+  let recipe = {
+    description: 'yummy chicken noodle soup',
+    ingredients: [
+      {quantity: "1 cup", name: 'chicken'},
+      {quantity: "3 nanoliters", name: 'stock'},
+      {quantity: "12", name: 'noodles'}
+    ]
+  }
+
+  let template = document.getElementById("my-template").innerHTML;
+  let templateFn = Handlebars.compile(template);
+  let html = templateFn(recipe);
   //put any page initialization/handlebars initialization here
 }
 document.addEventListener("DOMContentLoaded", function(event) {
